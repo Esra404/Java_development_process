44cf8bba27b097854471be94eee4766ba6bb230f
Esra404 <esradrmz571@gmail.com>
2023-11-04 02:38:45 +0300

    initial commit

---
+++ patika/donguler/whileDongu.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class whileDongu {
+    Scanner scanner = new Scanner(System.in);
+    String customerPassword = "12345";
+    boolean passwordSuccessfull = false;
+
+while(!passwordSuccessfull) {
+
+        System.out.println("Hesap şifrenizi giriniz:");
+        String typedPassword = scanner.next();
+
+        if(customerPassword.contentEquals(typedPassword)) {
+            passwordSuccessfull = true;
+            System.out.println("Sisteme başarılı giriş yaptınız!");
+        }
+    }
+}
