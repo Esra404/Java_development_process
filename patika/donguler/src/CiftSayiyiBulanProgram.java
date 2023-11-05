17aff214b01087589fbddc2c28242791f03a96d9
Esra404 <esradrmz571@gmail.com>
2023-11-05 23:14:13 +0300

    çift sayıyı bulan algoritma

--- patika/donguler/src/CiftSayiyiBulanProgram.java
+++ patika/donguler/src/CiftSayiyiBulanProgram.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class CiftSayiyiBulanProgram {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        /*
-        * Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan
-        *  3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.*/
+       //Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan programı yazıyoruz.
 
-        int ortalama;
         int toplam=0;
         System.out.println("lütfen bir sayı giriniz");
         int sayi=scanner.nextInt();
 
         for(int i=0;i<=sayi;i++){
-            if(sayi%3==0 && sayi%4 ==0){
-                toplam +=sayi;
+            if(i%2==0){
+                toplam  +=i ;
 
             }
         }
