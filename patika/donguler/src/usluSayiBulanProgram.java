913ae4dc022c9b3b29c7a3b5e319aee7024f8757
Esra404 <esradrmz571@gmail.com>
2023-11-06 03:29:45 +0300

    faktoriyel hesaplayan algoritmalar

---
+++ patika/donguler/src/usluSayiBulanProgram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class usluSayiBulanProgram {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("üsü alınacak sayi");
+        int sayi=scanner.nextInt();
+        System.out.println("üs olunacak sayi");
+        int us= scanner.nextInt();
+
+
+    }
+}
