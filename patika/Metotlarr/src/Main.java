8415832e32b7896814e2b268406140025ed6ea20
Esra404 <esradrmz571@gmail.com>
2023-12-20 20:33:06 +0300

    algoritma örnekleri çözümledim

---
+++ patika/Metotlarr/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+
+    static  int power(int base,int exp){
+        int result=1;
+        for(int i=1;i<=exp;i++){
+            result *=base;
+        }
+        return  result;
+    }
+    public static void main(String[] args) {
+
+        int case1=power(2,3);
+        System.out.println(case1);
+
+    }
+}
---
+++ patika/Metotlarr/src/ornek1.java
@@ -0,0 +1,2 @@
+public class ornek1 {
+}
