675a482d2d65e2d18c14ccce0763844ec85ea2c1
Esra404 <esradrmz571@gmail.com>
2023-12-22 00:29:48 +0300

    car sınıfını incleme

--- patika/Metotlarr/src/Main.java
+++ patika/Metotlarr/src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Car audi=new Car();
-        audi.model="audi";
-        audi.speed=10;
+        Car audi=new Car("suports","audi","red");
+
         audi.increaseSpeed(20);
         audi.decreaseSpeed(10);
-        audi.printSpeed();
-        Car bmw=new Car();
-        bmw.model="bmw";
-        bmw.speed=20;
+        audi.printInfo();
+
+
+        Car bmw=new Car("suports","bmw","blue");
+
         bmw.increaseSpeed(20);
         bmw.increaseSpeed(25);
         bmw.increaseSpeed(120);
-        bmw.printSpeed();
-        System.out.println("fd");
-        Car mercedes=new Car();
-        mercedes.model="mercedes";
-        mercedes.speed=30;
-        mercedes.printSpeed();
+        bmw.printInfo();
+
+
+
+        Car mercedes=new Car("corporate","mercedes","black");
+        mercedes.printInfo();
 
     }
 }
