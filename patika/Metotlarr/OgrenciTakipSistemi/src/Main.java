4b0c369fc2e96c67599787884402b5e2dfbf136d
Esra404 <esradrmz571@gmail.com>
2023-12-22 00:40:22 +0300

    not sistemi uygulaması

---
+++ patika/Metotlarr/OgrenciTakipSistemi/src/Main.java
@@ -0,0 +1,9 @@
+import javax.xml.namespace.QName;
+
+public class Main {
+    public static void main(String[] args) {
+        Teacher t1=new Teacher("mahmut hoca","TRH","0500000");
+        t1.print();
+
+    }
+}
